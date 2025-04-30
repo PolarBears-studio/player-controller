64dcdbb0098a702024ab2994e66ca3ccb5328187
Thomas Stucky <thomas@astrostucky.com>
2025-04-29 22:04:32 -0600

    Finalized Gravity.cs and removed recommendation comment

--- addons/player_controller/Scripts/Gravity.cs
+++ addons/player_controller/Scripts/Gravity.cs
@@ -4,18 +4,11 @@ namespace Exodus.Scripts.Player.PlayerController;
 
 public partial class Gravity: Node3D
 {
-	// NOTE: I don't find this API very intuitive. Weight is actually being used
-	// as mass, I do not know why it is being divided by 30, StartVelocity is not
-	// being used as a velocity at all, and it's impossible for me to know the
-	// units or scale of AdditionalGravityPower.
-	// I am viewing this from a physics perspective. Maybe this is a perfectly
-	// normal formula game dev's use I just never encountered.
-
-	[Export(PropertyHint.Range, "0,100,,or_greater")]
+	[Export(PropertyHint.Range, "0,100,0.1,or_greater")]
 	public float Weight { get; set; } = 70.0f;
-	[Export(PropertyHint.Range, "0,20,,or_greater")]
+	[Export(PropertyHint.Range, "0,20,0.1,or_greater")]
 	public float StartVelocity { get; set; } = 3.0f;
-	[Export(PropertyHint.Range, "0.1,10,,or_greater")]
+	[Export(PropertyHint.Range, "0.1,10,0.1,or_greater")]
 	public float AdditionalGravityPower { get; set; } = 2f;
 
 	private float _gravity;
@@ -26,5 +19,5 @@ public partial class Gravity: Node3D
 	}
 
 	public float CalculateJumpForce() => Weight * (_gravity * (StartVelocity / AdditionalGravityPower));
-	public float CalculateGravityForce() => _gravity * Weight / 30;
+	public float CalculateGravityForce() => _gravity * Weight / 30.0f;
 }
